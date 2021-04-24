c966e1866f550a9b5a6418ab13fae3ff8a8f179a
Marcos Vinicios <viniciosdinis@gmail.com>
2021-04-24 18:47:59 +0100

    Add project files.

---
+++ QRCodeAPI/Controllers/QrCodeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCodeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrCodeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetByUrl([FromQuery] string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                url = "https://medium.com/@marcosvinicios_net";
+            }
+            var image = QrCodeGenerator.GenerateByteArray(url);
+            return File(image, "image/jpeg");
+        }
+    }
+}
---
+++ QRCodeAPI/QrCodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCodeAPI
+{
+    public static class QrCodeGenerator
+    {
+        public static Bitmap GenerateImage(string url)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(10);
+            return qrCodeImage;
+        }
+
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenerateImage(url);
+            return ImageToByte(image);
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            using var stream = new MemoryStream();
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+    }
+}
